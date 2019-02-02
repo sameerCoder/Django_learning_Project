b2124e227d6afa6bee9fc8f3fb5e2f8ef673c0f0
sameerCoder <chatwithnubia2@gmial.com>
2019-02-02 12:58:10 +0000

    first commit

---
+++ learning_user/basicapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from basicapp import views
+from django.urls import path
+
+
+app_name='basicapp'
+
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+
+]
